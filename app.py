4a0703f36c5f9532da757344ed6e9a2f4a4b0e34
Sahil Mukkawar <mukkawarsahil99@gmail.com>
2025-03-06 22:47:31 +0530

    port error solved

--- app.py
+++ app.py
@@ -2754,4 +2754,5 @@ def add_admin():
 
 # Run the app
 if __name__ == '__main__':
-    socketio.run(app, debug=False, port=5000)
+    port = int(os.environ.get("PORT", 5001)) 
+    app.run(host="0.0.0.0", port=port)
